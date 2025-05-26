dbe16e0a19f85bf8c197cab5d7961b25676a4d53
klb346 <klb346@cornell.edu>
2025-05-26 13:36:02 -0400

    updates klb

--- src/MRFM_BrownianFit/LabVIEW_int.py
+++ src/MRFM_BrownianFit/LabVIEW_int.py
@@ -77,17 +77,17 @@ class LVprocessing():
                 doc.append(self.NoEscape("The position-fluctuation power spectrum was fit to the equation:"))
                 doc.append("\n")
                 doc.append(self.NoEscape("$P(f) = \\frac{kT \\tau_0^2}{\Gamma}\\frac{1}{(\pi\\tau_0)^4(f_0^2-f^2)^2+(\pi\\tau_0)^2f^2} + S_{x}$"))
-                doc.append("\n")
+                doc.append("\n\n")
                 doc.append(self.NoEscape("From the optimized fit paramters $\\tau_{0}$, $f_{0}$, $/Gamma$, and $S_{x}$:"))
                 doc.append("\n")
-                doc.append(self.NoEscape("$Q = \pi \\tau_0 f_0$"))
+                doc.append(self.NoEscape("$Q = \pi  \\tau_0  f_0$"))
                 doc.append("\n")
-                doc.append(self.NoEscape("$k = 2 \pi^2 f_0^2 \\tau_0 \Gamma$"))
+                doc.append(self.NoEscape("$k = 2  \pi^2  f_0^2  \\tau_0  \Gamma$"))
                 doc.append("\n")
                 #doc.append(self.NoEscape("$P(0) = frac{kt}{\Gamma Q^4}"))
-                doc.append(self.NoEscape("$S_th = frac{kT * \\tau_0^2}{(\pi * \\tau_0)^4 f_0 ^4}$"))
+                doc.append(self.NoEscape("$S_{th} = \\frac{kT \\tau_0^2}{(\pi  \\tau_0)^4  f_0^4}$"))
                 doc.append("\n")
-                doc.append(self.NoEscape("$S_det = frac{S_x}{z_{rms}^2}$ where $z_{rms} = 0.05 pm$"))
+                doc.append(self.NoEscape("$S_{det} = \\frac{S_x}{z_{rms}^2}$ where $z_{rms} = 0.05  pm$"))
                 doc.append("\n")
 
                 #number averages
@@ -99,42 +99,42 @@ class LVprocessing():
 
                 #freq range for fit
                 doc.append("Power Spectrum fit to frequency range " + str(self.fit.x_trunc[0]) + " to " + str(self.fit.x_trunc[-1]) + " Hz")
-                doc.append("\n")
+                doc.append("\n\n")
                 
                 #the fit parameters
                 doc.append("From curve fitting the position-fluctuation power spectrum:")
                 doc.append("\n")
                 #tau_0 with error - ringdown time
-                doc.append(self.NoEscape("$\\tau_0 =$ " + str(self.fit.tau0) + "$\pm$" + str(self.fit.tau0_stderr) + " s"))
+                doc.append(self.NoEscape("$\\tau_0 =$ " + str(round(self.fit.tau0,4)) + "$\pm$" + str(round(self.fit.tau0_stderr,4)) + " s"))
                 doc.append("\n")
                 #f0 with error - resonance freq
-                doc.append(self.NoEscape("$f_0 =$ " + str(self.fit.f0) + "$\pm$" + str(self.fit.f0_stderr) + " Hz"))
+                doc.append(self.NoEscape("$f_0 =$ " + str(round(self.fit.f0,2)) + "$\pm$" + str(round(self.fit.f0_stderr, 2)) + " Hz"))
                 doc.append("\n")
                 #gamma with error - friction coefficent
-                doc.append(self.NoEscape("$\Gamma =$ " + str(self.fit.result['brownian'].best_values['Gamma']) + "$\pm$" + str(self.fit.result['brownian'].params['Gamma'].stderr) + " N s/m"))
+                doc.append(self.NoEscape("$\Gamma =$ " + str(round(self.fit.result['brownian'].best_values['Gamma'] * 1E12,5)) + "$\pm$" + str(round(self.fit.result['brownian'].params['Gamma'].stderr * 1E12,5)) + " pN s/m"))
                 doc.append("\n")
                 #Sx with error -(detector noise floor)
-                doc.append(self.NoEscape("$S_x =$ " + str(self.fit.Sx) + "$\pm$" + str(self.fit.Sx_stderr) + " $nm^2$/Hz"))
+                doc.append(self.NoEscape("$S_x =$ " + str(round(self.fit.Sx * 1E6,5)) + "$\pm$" + str(round(self.fit.Sx_stderr * 1E6, 5)) + " ${\mu m}^2$/Hz"))
                 doc.append("\n")
 
                 #Q with error
-                doc.append(self.NoEscape("$Q =$ " + str(self.fit.Q) + "$\pm$" + str(self.fit.Q_stderr)))
+                doc.append(self.NoEscape("$Q =$ " + str(round(self.fit.Q,2)) + "$\pm$" + str(round(self.fit.Q_stderr,2))))
                 doc.append("\n")
                 #Px(0) with error - zero frequency
-                doc.append(self.NoEscape("$P_0 =$ " + str(self.fit.Q) + "$\pm$" + str(self.fit.Q_stderr)))
-                doc.append("\n")
+                doc.append(self.NoEscape("$P_0 =$ " + str(self.fit.P0) + "$\pm$" + str(self.fit.P0_stderr) + " $nm^2$/Hz"))
+                doc.append("\n\n")
 
                 doc.append("From analysis of the area under the position-fluctuation power spectrum:")
                 doc.append("\n")
                 #k with error
-                doc.append(self.NoEscape("$k =$ " + str(self.fit.P0) + "$\pm$" + str(self.fit.P0_stderr) + " $nm^2$/Hz"))
+                doc.append(self.NoEscape("$k =$ " + str(round(self.fit.k,5)) + "$\pm$" + str(round(self.fit.k_stderr,5)) + " mN/m"))
                 doc.append("\n")
                 # intrinsic dissipation
                 doc.append((self.NoEscape("Intrinsic dissapation = " + str(self.fit.intrinsic_diss) + "$\pm$" + str(self.fit.intrinsic_diss_stderr) + " pN s/m")))
                 doc.append("\n")
                 #intrinsic force noise
                 doc.append((self.NoEscape("Intrinsic force noise = " + str(self.fit.intrinsic_force_noise) + "$\pm$" + str(self.fit.intrinsic_force_noise_stderr)  + " $aN^2$/Hz")))
-                doc.append("\n")
+                doc.append("\n\n")
 
                 doc.append("Predicted frequency noise:")
                 doc.append("\n")
